ef65a65111718729b4574137b9bf257e1a5fa21b
Cimino, Brandon 341 <Brandon.Cimino@sysco.com>
2020-04-09 16:16:57 -0500

    Stubbed IncrementalUtils.cs Added script & test directories & .asmdef files

---
+++ Assets/Scripts/Editor/IncrementalUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Contains utility methods that manage general "Incremental Game" functionality,
+/// such as calculating the number of "grabs" that have happened in a given time period.
+///
+/// Because this is a "Utility" class, methods should be <c>static</c> whenever possible.
+/// </summary>
+public class IncrementalUtils
+{
+    public static int NumberOfGrabs(float grabInterval, float duration)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int NumberOfGrabs(float grabInterval, long startTime, long endTime)
+    {
+        throw new NotImplementedException();
+    }
+}
