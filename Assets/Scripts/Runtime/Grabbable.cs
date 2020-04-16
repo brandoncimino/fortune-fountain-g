f9c2fbadcc3cf74f8a87a8cda9a604a9850a5f76
GuyWithPasta <guywithpasta@gmail.com>
2020-04-16 12:54:42 -0500

    Added basic functionality of grabbable items

---
+++ Assets/Scripts/Runtime/Grabbable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+// ReSharper disable CheckNamespace
+
+namespace DefaultNamespace
+{
+    /// <summary>
+    /// A class containing the information of a type of item able to be grabbed (pennies, nickles, etc.)
+    /// </summary>
+    public class Grabbable
+    {
+        private Grabbable()
+        {
+        }
+        public Grabbable(string name, int basePrice, int maxStack, float grabInterval)
+        {
+            Name = name;
+            this.basePrice = basePrice;
+            this.maxStack = maxStack;
+            this.grabInterval = grabInterval;
+        }
+        
+        private string Name = "null";
+        public int basePrice = 0;
+        public int maxStack = Int32.MaxValue;
+        public float grabInterval;
+    }
+
+    public class GrabbedItems
+    {
+        private Grabbable Item;
+        public int value;
+
+        public GrabbedItems(Grabbable grabbable, int value)
+        {
+            Item = grabbable;
+            this.value = value;
+        }
+    }
+}
