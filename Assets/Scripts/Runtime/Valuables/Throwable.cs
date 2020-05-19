c19447d753db791d8e64fb2a8a02c01f4d45a95a
brandoncimino <brandon.cimino@gmail.com>
2020-05-19 10:50:30 -0500

    Added Throwable.cs

---
+++ Assets/Scripts/Runtime/Valuables/Throwable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Runtime.Valuables
+{
+    public class Throwable
+    {
+        [SerializeField] public ValuableType ValuableType;
+
+        [SerializeField] public double ThrowValue;
+
+        public Throwable(ValuableType valuableType, double throwValue)
+        {
+            ValuableType = valuableType;
+            ThrowValue = throwValue;
+        }
+    }
+}
