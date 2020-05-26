081603ebf122a5cd1e4f42a76c6a5b29717c3be8
brandoncimino <brandon.cimino@gmail.com>
2020-05-26 10:35:06 -0500

    - Added some doc comments to SaveDataException.cs - Added ReSaveDelayException.cs

--- Packages/BrandonUtils/Runtime/Saving/SaveDataException.cs
+++ Packages/BrandonUtils/Runtime/Saving/SaveDataException.cs
@@ -1,52 +1,42 @@
 ﻿using System;
 using Packages.BrandonUtils.Runtime.Exceptions;
 
-namespace Packages.BrandonUtils.Runtime.Saving
-{
-    public class SaveDataException<T> : BrandonException where T : SaveData<T>, new()
-    {
+namespace Packages.BrandonUtils.Runtime.Saving {
+    /// <summary>
+    /// A special <see cref="BrandonException"/> for exceptions caused by <see cref="SaveData{t}"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SaveDataException<T> : BrandonException where T : SaveData<T>, new() {
         private SaveData<T> saveData;
 
-        public SaveDataException(SaveData<T> saveData) : base(GetMessage(saveData))
-        {
+        public SaveDataException(SaveData<T> saveData) : base(GetMessage(saveData)) {
             this.saveData = saveData;
         }
 
-        public SaveDataException(SaveData<T> saveData, string message) : base(GetMessage(saveData, message))
-        {
+        public SaveDataException(SaveData<T> saveData, string message) : base(GetMessage(saveData, message)) {
             this.saveData = saveData;
         }
 
         public SaveDataException(SaveData<T> saveData, Exception innerException) : base(GetMessage(saveData),
-            innerException)
-        {
+                                                                                        innerException) {
             this.saveData = saveData;
         }
 
-        public SaveDataException(Exception innerException) : base(GetMessage(), innerException)
-        {
-        }
+        public SaveDataException(Exception innerException) : base(GetMessage(), innerException) { }
 
         public SaveDataException(string message, Exception innerException) : base(GetMessage(message: message),
-            innerException)
-        {
-        }
+                                                                                  innerException) { }
 
-        public SaveDataException(string message) : base(GetMessage(message: message))
-        {
-        }
+        public SaveDataException(string message) : base(GetMessage(message: message)) { }
 
         public SaveDataException(SaveData<T> saveData, string message, Exception innerException) : base(
-            GetMessage(saveData, message), innerException)
-        {
+            GetMessage(saveData, message), innerException) {
             this.saveData = saveData;
         }
 
         private static string GetMessage(SaveData<T> saveData = null,
-            string message = "Something went wrong with save data management!")
-        {
-            if (saveData != null)
-            {
+                                         string message = "Something went wrong with save data management!") {
+            if (saveData != null) {
                 message += $"\nnickName: {saveData.nickName}\nData:\n{saveData}";
             }
 
