6ae01db5d7cf23676a90823143dbf4c958a18ca2
brandoncimino <brandon.cimino@gmail.com>
2020-05-26 12:02:24 -0500

    Added a simple variable to SaveDataTestImpl.cs

--- Packages/BrandonUtils/Runtime/Saving/SaveDataTestImpl.cs
+++ Packages/BrandonUtils/Runtime/Saving/SaveDataTestImpl.cs
@@ -1,12 +1,14 @@
-﻿namespace Packages.BrandonUtils.Runtime.Saving
-{
+﻿using System;
+
+namespace Packages.BrandonUtils.Runtime.Saving {
     /// <summary>
     ///     An example implementation of <see cref="SaveData{T}"/>.
     /// </summary>
     /// <remarks>
     ///     Intended only for use by unit tests.
     /// </remarks>
-    public class SaveDataTestImpl : SaveData<SaveDataTestImpl>
-    {
+    [Serializable]
+    public class SaveDataTestImpl : SaveData<SaveDataTestImpl> {
+        public string Word;
     }
 }
