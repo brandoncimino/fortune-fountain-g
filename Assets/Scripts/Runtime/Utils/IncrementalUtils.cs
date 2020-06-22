8189dc4b2b7c881f691e32b35d921b2686a51e18
brandoncimino <brandon.cimino@gmail.com>
2020-06-22 12:08:10 -0500

    Reformatted & properly updated namespace

--- Assets/Scripts/Runtime/Utils/IncrementalUtils.cs
+++ Assets/Scripts/Runtime/Utils/IncrementalUtils.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 
-namespace Runtime
-{
+namespace Runtime.Utils {
     /// <summary>
     /// Contains utility methods that manage general "Incremental Game" functionality,
     /// such as calculating the number of "grabs" that have happened in a given time period.
     ///
     /// Because this is a "Utility" class, methods should be <c>static</c> whenever possible.
     /// </summary>
-    public class IncrementalUtils
-    {
-        public static int NumberOfGrabs(float grabInterval, float duration)
-        {
+    public class IncrementalUtils {
+        public static int NumberOfGrabs(float grabInterval, float duration) {
             throw new NotImplementedException();
         }
 
-        public static int NumberOfGrabs(float grabInterval, long startTime, long endTime)
-        {
+        public static int NumberOfGrabs(float grabInterval, long startTime, long endTime) {
             throw new NotImplementedException();
         }
     }
