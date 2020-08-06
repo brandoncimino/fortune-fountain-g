9395eb77f7d2ad0efb7e0fb7f3d2bae00e715e9e
brandoncimino <brandon.cimino@gmail.com>
2020-08-06 11:22:03 -0500

    Added PlayerValuableTests.cs

---
+++ Assets/Tests/Runtime/PlayerValuableTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Runtime.Saving;
+using Runtime.Valuables;
+
+namespace Tests.Runtime {
+    public class PlayerValuableTests {
+        [Test]
+        public void TestFirstValuableEnabledOnNewSave() {
+            var nickName = nameof(TestFirstValuableEnabledOnNewSave);
+            FortuneFountainSaveData fortuneFountainSaveData = FortuneFountainSaveData.NewSaveFile(nickName);
+
+            Assert.That(fortuneFountainSaveData.PlayerValuables.Keys, Contains.Key(0), $"The new save file didn't contain PlayerValuable type 0 ({(ValuableType) 0})!");
+            Assert.That(fortuneFountainSaveData.PlayerValuables.Keys.Count, Is.EqualTo(1), "The save file contained extra player valuables!");
+        }
+    }
+}
