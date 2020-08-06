072bd32788fa55a1b06b0fd16208439dbc20f8ca
brandoncimino <brandon.cimino@gmail.com>
2020-08-06 10:41:16 -0500

    Replaced stubbed NumberOfGrabs methods with the more general and accurate NumberOfTimesCompleted()

--- Assets/Scripts/Runtime/Utils/IncrementalUtils.cs
+++ Assets/Scripts/Runtime/Utils/IncrementalUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Packages.BrandonUtils.Runtime;
 
 namespace Runtime.Utils {
     /// <summary>
@@ -8,12 +9,25 @@ namespace Runtime.Utils {
     /// Because this is a "Utility" class, methods should be <c>static</c> whenever possible.
     /// </summary>
     public class IncrementalUtils {
-        public static int NumberOfGrabs(float grabInterval, float duration) {
-            throw new NotImplementedException();
-        }
-
-        public static int NumberOfGrabs(float grabInterval, long startTime, long endTime) {
-            throw new NotImplementedException();
+        /// <summary>
+        /// Calculates the number of times that <paramref name="duration"/> has <b>completely</b> occurred within the span of <paramref name="startTime"/> to <paramref name="endTime"/>.
+        /// <br/>
+        /// Stores the time of the <b>last full completion</b> of <paramref name="duration"/> in <paramref name="lastCompletionTime"/>.
+        /// </summary>
+        /// <remarks>
+        /// <paramref name="lastCompletionTime"/> can be used as the <paramref name="startTime"/> in future calls to <see cref="NumberOfTimesCompleted"/>.
+        /// <br/>
+        /// Returns a <c>double</c> (as opposed to a <c>long</c> or <c>int</c>) to mimic methods such as <see cref="Math.Floor(double)"/>, which return <c>double</c> values despite the results expected to be whole numbers.
+        /// </remarks>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        /// <param name="duration"></param>
+        /// <param name="lastCompletionTime"></param>
+        /// <returns></returns>
+        public static double NumberOfTimesCompleted(DateTime startTime, DateTime endTime, TimeSpan duration, out DateTime lastCompletionTime) {
+            var deltaTime = endTime - startTime;
+            lastCompletionTime = endTime - deltaTime.QuotientSpan(duration);
+            return deltaTime.Quotient(duration);
         }
     }
 }
