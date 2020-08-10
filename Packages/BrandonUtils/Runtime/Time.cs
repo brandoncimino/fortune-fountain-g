361b072cd9becfbf84a47014488e63b29621d4cd
brandoncimino <brandon.cimino@gmail.com>
2020-08-10 13:05:53 -0500

    Added clearer validations for divide by zero stuff.

--- Packages/BrandonUtils/Runtime/Time.cs
+++ Packages/BrandonUtils/Runtime/Time.cs
@@ -3,51 +3,60 @@
 namespace Packages.BrandonUtils.Runtime {
     public static class Time {
         /// <summary>
-        /// Mimics .NET Core's <a href="https://docs.microsoft.com/en-us/dotnet/api/system.timespan.divide">TimeSpan.Divide</a>.
-        ///
-        /// Does this by converting the given <see cref="TimeSpan"/>s into ticks and performing the division on those.
+        ///     Mimics .NET Core's <a href="https://docs.microsoft.com/en-us/dotnet/api/system.timespan.divide">TimeSpan.Divide</a>.
+        ///     Does this by converting the given <see cref="TimeSpan" />s into <see cref="TimeSpan.TotalSeconds" /> and performing the division on those.
         /// </summary>
+        /// <remarks>
+        ///     This originally performed the division on <see cref="TimeSpan.Ticks" /> rather than <see cref="TimeSpan.TotalSeconds" />, but this was actually slightly inaccurate due to the number of ticks being so large.
+        /// </remarks>
         /// <param name="dividend">The number to be divided (i.e. top of the fraction)</param>
-        /// <param name="divisor">The number by which <paramref name="dividend"/> will be divided (i.e. the bottom of the fraction)</param>
+        /// <param name="divisor">The number by which <paramref name="dividend" /> will be divided (i.e. the bottom of the fraction)</param>
         /// <returns></returns>
         public static double Divide(this TimeSpan dividend, TimeSpan divisor) {
-            //cast of dividend.Ticks to (double) is to prevent loss of fractions
-            return (double) dividend.Ticks / divisor.Ticks;
+            ValidateDivisor(divisor);
+            return dividend.TotalSeconds / divisor.TotalSeconds;
         }
 
         /// <summary>
-        /// Divides <paramref name="dividend"/> by <paramref name="divisor"/>, returning the integer quotient.
+        ///     Divides <paramref name="dividend" /> by <paramref name="divisor" />, returning the integer quotient.
         /// </summary>
         /// <param name="dividend">The number to be divided (i.e. top of the fraction)</param>
-        /// <param name="divisor">The number by which <paramref name="dividend"/> will be divided (i.e. the bottom of the fraction)</param>
+        /// <param name="divisor">The number by which <paramref name="dividend" /> will be divided (i.e. the bottom of the fraction)</param>
         /// <returns></returns>
         public static double Quotient(this TimeSpan dividend, TimeSpan divisor) {
+            ValidateDivisor(divisor);
             return Math.Floor(Divide(dividend, divisor));
         }
 
         /// <summary>
-        /// Returns the <see cref="TimeSpan"/> remainder after <paramref name="dividend"/> is divided by <paramref name="divisor"/>.
+        ///     Returns the <see cref="TimeSpan" /> remainder after <paramref name="dividend" /> is divided by <paramref name="divisor" />.
         /// </summary>
         /// <param name="dividend">The number to be divided (i.e. top of the fraction)</param>
-        /// <param name="divisor">The number by which <paramref name="dividend"/> will be divided (i.e. the bottom of the fraction)</param>
-        /// <seealso cref="QuotientSpan"/>
+        /// <param name="divisor">The number by which <paramref name="dividend" /> will be divided (i.e. the bottom of the fraction)</param>
+        /// <seealso cref="QuotientSpan" />
         /// <returns></returns>
         public static TimeSpan Modulus(this TimeSpan dividend, TimeSpan divisor) {
+            ValidateDivisor(divisor);
             return TimeSpan.FromTicks(dividend.Ticks % divisor.Ticks);
         }
 
         /// <summary>
-        /// Performs division similar to <see cref="Quotient"/>, but returns the result as a <see cref="TimeSpan"/>.
+        ///     Performs division similar to <see cref="Quotient" />, but returns the result as a <see cref="TimeSpan" />.
         /// </summary>
         /// <remarks>
-        /// Should be equivalent to multiplying <paramref name="dividend"/> by the result of <see cref="Quotient"/>, but is simpler in its actual execution.
+        ///     Should be equivalent to multiplying <paramref name="dividend" /> by the result of <see cref="Quotient" />, but is simpler in its actual execution.
         /// </remarks>
         /// <param name="dividend">The number to be divided (i.e. top of the fraction)</param>
-        /// <param name="divisor">The number by which <paramref name="dividend"/> will be divided (i.e. the bottom of the fraction)</param>
-        /// <seealso cref="Modulus"/>
+        /// <param name="divisor">The number by which <paramref name="dividend" /> will be divided (i.e. the bottom of the fraction)</param>
+        /// <seealso cref="Modulus" />
         /// <returns></returns>
         public static TimeSpan QuotientSpan(this TimeSpan dividend, TimeSpan divisor) {
+            ValidateDivisor(divisor);
             return TimeSpan.FromTicks(dividend.Ticks / divisor.Ticks);
+        }
+
+        private static void ValidateDivisor(TimeSpan divisor) {
+            if (divisor == TimeSpan.Zero) throw new DivideByZeroException("Cannot divide by a zero TimeSpan!");
         }
     }
 }
--- Packages/BrandonUtils/Tests/TimeTests.cs
+++ Packages/BrandonUtils/Tests/TimeTests.cs
@@ -1,57 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
 using NUnit.Framework;
 using Packages.BrandonUtils.Runtime;
 
+// ReSharper disable CompareOfFloatsByEqualityOperator
+
 namespace Packages.BrandonUtils.Tests {
     public class TimeTests {
-        private class DivTest {
-            public double dividendSeconds;
-            public double divisorSeconds;
-
-            public TimeSpan dividend => TimeSpan.FromSeconds(dividendSeconds);
-            public TimeSpan divisor => TimeSpan.FromSeconds(divisorSeconds);
-
-            public DivTest(double dividendSeconds, double divisorSeconds) {
-                this.dividendSeconds = dividendSeconds;
-                this.divisorSeconds = divisorSeconds;
-            }
-        }
-
-        /// <summary>
-        /// NOTE: There are limits to these values based on the precision of TimeSpan.Seconds
-        /// </summary>
-        private List<DivTest> DivTestData = new List<DivTest> {
-            new DivTest(5d, 2),
-            new DivTest(0d, 10),
-            new DivTest(0.53, 2),
-            new DivTest(264576.523, 7801.623)
+        private static double[] valuesInSeconds = {
+            5d,
+            0d,
+            0.53,
+            2,
+            10,
+            264576.523,
+            7801.623
         };
 
         [Test]
-        public void TestDivision() {
-            foreach (var test in DivTestData) {
-                Assert.That(test.dividend.Divide(test.divisor), Is.EqualTo(test.dividendSeconds / test.divisorSeconds));
-            }
+        [Combinatorial]
+        public void TestDivision(
+            [ValueSource(nameof(valuesInSeconds))] double dividendSeconds,
+            [ValueSource(nameof(valuesInSeconds))] double divisorSeconds
+        ) {
+            if (divisorSeconds == 0) Assert.Ignore("Checking for division by zero is a different test!");
+
+            var dividend = TimeSpan.FromSeconds(dividendSeconds);
+            var divisor = TimeSpan.FromSeconds(divisorSeconds);
+
+            Assert.That(dividend.Divide(divisor), Is.EqualTo(dividendSeconds / divisorSeconds));
         }
 
         [Test]
-        public void TestQuotient() {
-            foreach (var test in DivTestData) {
-                Assert.That(test.dividend.Quotient(test.divisor), Is.EqualTo(Math.Floor(test.dividendSeconds / test.divisorSeconds)));
-            }
+        public void TestDivisionByZero([ValueSource(nameof(valuesInSeconds))] double dividendSeconds) {
+            Assert.Throws<DivideByZeroException>(() => TimeSpan.FromSeconds(dividendSeconds).Divide(TimeSpan.Zero));
         }
 
         [Test]
-        public void TestModulus() {
-            foreach (var test in DivTestData) {
-                var expectedModulus = TimeSpan.FromSeconds(test.dividendSeconds % test.divisorSeconds);
-                Assert.That(
-                    test.dividend.Modulus(test.divisor),
-                    Is.EqualTo(expectedModulus),
-                    $"The modulus of {test.dividendSeconds} % {test.divisorSeconds} = {expectedModulus}"
-                );
-            }
+        [Combinatorial]
+        public void TestQuotient(
+            [ValueSource(nameof(valuesInSeconds))] double dividendSeconds,
+            [ValueSource(nameof(valuesInSeconds))] double divisorSeconds
+        ) {
+            if (divisorSeconds == 0) Assert.Ignore("Checking for division by zero is a different test!");
+
+            var dividend = TimeSpan.FromSeconds(dividendSeconds);
+            var divisor = TimeSpan.FromSeconds(divisorSeconds);
+
+            Assert.That(dividend.Quotient(divisor), Is.EqualTo(Math.Floor(dividendSeconds / divisorSeconds)));
+        }
+
+        [Test]
+        public void TestQuotientByZero([ValueSource(nameof(valuesInSeconds))] double dividendSeconds) {
+            Assert.Throws<DivideByZeroException>(() => TimeSpan.FromSeconds(dividendSeconds).Quotient(TimeSpan.Zero));
+        }
+
+        [Test]
+        [Combinatorial]
+        public void TestModulus(
+            [ValueSource(nameof(valuesInSeconds))] double dividendSeconds,
+            [ValueSource(nameof(valuesInSeconds))] double divisorSeconds
+        ) {
+            if (divisorSeconds == 0) Assert.Ignore("The divisor must not be 0 - that's a different test.");
+
+            var dividend = TimeSpan.FromSeconds(dividendSeconds);
+            var divisor = TimeSpan.FromSeconds(divisorSeconds);
+            var expectedModTicks = dividend.Ticks % divisor.Ticks;
+            var expectedModSpan = TimeSpan.FromTicks(expectedModTicks);
+
+            Assert.That(
+                dividend.Modulus(divisor),
+                Is.EqualTo(expectedModSpan),
+                $"The modulus of {dividend} % {divisor} = {expectedModSpan}"
+            );
+        }
+
+        [Test]
+        public void TestModulusByZero([ValueSource(nameof(valuesInSeconds))] double dividendSeconds) {
+            Assert.Throws<DivideByZeroException>(() => TimeSpan.FromSeconds(dividendSeconds).Modulus(TimeSpan.Zero));
         }
     }
 }
