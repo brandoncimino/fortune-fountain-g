d047abee86c30168788c55f639c7eb6d3de12687
brandoncimino <brandon.cimino@gmail.com>
2020-08-11 09:56:20 -0500

    Added methods to compensate for decimal precision when converting to and from TimeSpans

--- Packages/BrandonUtils/Runtime/Time.cs
+++ Packages/BrandonUtils/Runtime/Time.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 
+// ReSharper disable MemberCanBePrivate.Global
+
 namespace Packages.BrandonUtils.Runtime {
     /// <summary>
     ///     Contains utility methods that manipulate or extend <see cref="DateTime" />, <see cref="TimeSpan" />, etc.
     /// </summary>
     public static class Time {
+        /// <summary>
+        ///     Corresponds to <a href="https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.datavisualization.charting.datetimeintervaltype?view=netframework-4.8">DateTimeIntervalType</a> and <a href="https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualbasic.dateinterval?view=netcore-3.1">DateInterval</a>, but I couldn't find a corresponding enum that was available inside of Unity.
+        /// </summary>
+        /// <remarks>
+        ///     <li>Specifically for use in <see cref="Time.NormalizePrecision" />.</li>
+        ///     <li>These should have parity with the <see cref="TimeSpan" /> methods like <see cref="TimeSpan.FromDays" />.</li>
+        /// </remarks>
+        public enum IntervalType {
+            Milliseconds,
+            Seconds,
+            Minutes,
+            Hours,
+            Days
+        }
+
         /// <summary>
         ///     Mimics .NET Core's <a href="https://docs.microsoft.com/en-us/dotnet/api/system.timespan.divide">TimeSpan.Divide</a>.
         ///     Does this by converting the given <see cref="TimeSpan" />s into <see cref="TimeSpan.TotalSeconds" /> and performing the division on those.
@@ -54,6 +71,69 @@ namespace Packages.BrandonUtils.Runtime {
         /// <returns></returns>
         public static TimeSpan Multiply(this TimeSpan timeSpan, double factor) {
             return TimeSpan.FromSeconds(timeSpan.TotalSeconds * factor);
+        }
+
+        /// <inheritdoc cref="NormalizePrecision" />
+        public static double NormalizeMinutes(double minutes) {
+            return TimeSpan.FromMinutes(minutes).TotalMinutes;
+        }
+
+        /// <inheritdoc cref="NormalizePrecision" />
+        public static double NormalizeSeconds(double seconds) {
+            return TimeSpan.FromSeconds(seconds).TotalSeconds;
+        }
+
+        /// <inheritdoc cref="NormalizePrecision" />
+        public static double NormalizeHours(double hours) {
+            return TimeSpan.FromHours(hours).TotalHours;
+        }
+
+        /// <inheritdoc cref="NormalizePrecision" />
+        public static double NormalizeDays(double days) {
+            return TimeSpan.FromDays(days).TotalDays;
+        }
+
+        /// <inheritdoc cref="NormalizePrecision" />
+        public static double NormalizeMilliseconds(double milliseconds) {
+            return TimeSpan.FromMilliseconds(milliseconds).TotalMilliseconds;
+        }
+
+        /// <summary>
+        ///     Reduces the given <paramref name="value" /> so that it matches the appropriate precision for the given <paramref name="unit" /> component of a <see cref="TimeSpan" />.
+        /// </summary>
+        /// <remarks>
+        ///     <li>Converts <paramref name="value" /> into a <see cref="TimeSpan" /> via the given <paramref name="unit" />, then returns the total <paramref name="unit" />s of the new <see cref="TimeSpan" />.</li>
+        ///     <li>
+        ///         Joins together the multiple "Normalize" methods, e.g. <see cref="NormalizeMinutes" />, into one method, via <see cref="IntervalType" />.
+        ///         <ul>
+        ///             <li>
+        ///                 The individual methods such as <see cref="NormalizeDays" /> are maintained for parity with <see cref="TimeSpan" /> methods such as <see cref="TimeSpan.FromDays" />.
+        ///             </li>
+        ///         </ul>
+        ///     </li>
+        /// </remarks>
+        /// <example>
+        ///     TODO: Add an example, because this is kinda hard to explain without one.
+        /// </example>
+        /// <param name="value"></param>
+        /// <param name="unit"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static double NormalizePrecision(double value, IntervalType unit) {
+            switch (unit) {
+                case IntervalType.Milliseconds:
+                    return NormalizeMilliseconds(value);
+                case IntervalType.Seconds:
+                    return NormalizeSeconds(value);
+                case IntervalType.Minutes:
+                    return NormalizeMinutes(value);
+                case IntervalType.Hours:
+                    return NormalizeHours(value);
+                case IntervalType.Days:
+                    return NormalizeDays(value);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(unit), unit, $"I don't know how to make a {nameof(TimeSpan)} out of {unit}s!");
+            }
         }
     }
 }
