d984d4345b9e62c38d3a9e3aff51afe09c8c0af9
brandoncimino <brandon.cimino@gmail.com>
2020-08-12 10:09:20 -0500

    Replaced String with string

--- Packages/BrandonUtils/Runtime/Logging/LogUtils.cs
+++ Packages/BrandonUtils/Runtime/Logging/LogUtils.cs
@@ -3,62 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Packages.BrandonUtils.Runtime.Logging
-{
+namespace Packages.BrandonUtils.Runtime.Logging {
     [RequireComponent(typeof(Text))]
-    public class LogUtils : MonoBehaviour
-    {
+    public class LogUtils : MonoBehaviour {
         [Flags]
-        public enum Locations
-        {
+        public enum Locations {
             Console,
             Unity,
             UI,
             All = Console | Unity | UI
         }
 
-        public static List<String> lines = new List<string>();
+        public static List<string> lines = new List<string>();
         public static Locations locations = Locations.All;
         private Text _text;
 
-        private void Awake()
-        {
+        private void Awake() {
             _text = GetComponent<Text>();
             Log($"Started at: {DateTime.Now} with locations {locations}");
         }
 
-        public static void Log(params object[] stuffToLog)
-        {
+        public static void Log(params object[] stuffToLog) {
             Log(null, stuffToLog);
         }
 
-        public static void Log(Color? color, params object[] stuffToLog)
-        {
-            foreach (var thing in stuffToLog)
-            {
-                lines.Add(Colorize(thing, color));
-            }
+        public static void Log(Color? color, params object[] stuffToLog) {
+            foreach (var thing in stuffToLog) lines.Add(Colorize(thing, color));
 
-            var joinedLines = String.Join("\n", stuffToLog);
+            var joinedLines = string.Join("\n", stuffToLog);
 
-            if (locations.HasFlag(Locations.Console))
-            {
-                Console.WriteLine(joinedLines);
-            }
+            if (locations.HasFlag(Locations.Console)) Console.WriteLine(joinedLines);
 
-            if (locations.HasFlag(Locations.Unity))
-            {
-                Debug.Log(joinedLines);
-            }
+            if (locations.HasFlag(Locations.Unity)) Debug.Log(joinedLines);
         }
 
-        public static string Colorize(object thing, Color? color)
-        {
+        public static string Colorize(object thing, Color? color) {
             return color == null ? $"{thing}" : $"<color=#{ColorUtility.ToHtmlStringRGB(color.Value)}>{thing}</color>";
         }
 
-        public void Update()
-        {
+        public void Update() {
             _text.enabled = locations.HasFlag(Locations.UI);
 
             _text.text = string.Join("\n", lines);
