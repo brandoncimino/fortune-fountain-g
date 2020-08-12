605870f14e08a96d663dd14f41c7110ec734f719
brandoncimino <brandon.cimino@gmail.com>
2020-08-12 13:03:31 -0500

    Added StringUtils.cs

---
+++ Packages/BrandonUtils/Runtime/StringUtils.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace Packages.BrandonUtils.Runtime {
+    public static class StringUtils {
+        /// <summary>
+        ///     Prepends <paramref name="toIndent" />.
+        /// </summary>
+        /// <param name="toIndent">The <see cref="string" /> to be indented.</param>
+        /// <param name="indentCount">The number of indentations (i.e. number of times hitting "tab").</param>
+        /// <param name="indentSize">The size of each individual indentation.</param>
+        /// <param name="indentChar">The character to use for indentation.</param>
+        /// <returns></returns>
+        public static string Indent(this string toIndent, int indentCount = 1, int indentSize = 2, char indentChar = ' ') {
+            return indentChar.ToString().Repeat(indentSize).Repeat(indentCount) + toIndent;
+        }
+
+        /// <summary>
+        ///     Joins <paramref name="toRepeat" /> with itself <paramref name="repetitions" /> times, using the optional <paramref name="separator" />
+        /// </summary>
+        /// <param name="toRepeat">The <see cref="string" /> to be joined with itself.</param>
+        /// <param name="repetitions">The number of times <paramref name="toRepeat" /> should be repeated.</param>
+        /// <param name="separator">An optional character, analogous to </param>
+        /// <returns></returns>
+        public static string Repeat(this string toRepeat, int repetitions, string separator = "") {
+            var list = new List<string>();
+            for (var i = 0; i < repetitions; i++) list.Add(toRepeat);
+
+            return string.Join(separator, list);
+        }
+
+        public static string Repeat(this char toRepeat, int repetitions, string separator = "") {
+            return Repeat(toRepeat.ToString(), repetitions, separator);
+        }
+
+        /// <summary>
+        ///     Joins together <paramref name="baseString" /> and <paramref name="stringToJoin" /> via <paramref name="separator" />,
+        ///     <b>
+        ///         <i>UNLESS</i>
+        ///     </b>
+        ///     <paramref name="baseString" /> is <c>null</c>, in which case <paramref name="stringToJoin" /> is returned.
+        /// </summary>
+        /// <remarks>
+        ///     The idea of this is that it can be used to build out a single string and "list out" items, rather than building a <see cref="List{T}" /> and calling <see cref="string.Join(string,System.Collections.Generic.IEnumerable{string})" /> against it.
+        /// </remarks>
+        /// <example>
+        ///     <code><![CDATA[
+        /// "yolo".Join("swag")     -> yoloswag
+        /// ]]></code>
+        ///     <code><![CDATA[
+        /// "yolo".Join("swag",":") -> yolo; swag
+        /// ]]></code>
+        ///     <code><![CDATA[
+        /// "".Join("swag", ":")    -> swag
+        /// ]]></code>
+        ///     <code><![CDATA[
+        /// null.Join(":")          -> swag
+        /// ]]></code>
+        /// </example>
+        /// <param name="baseString"></param>
+        /// <param name="stringToJoin"></param>
+        /// <param name="separator"></param>
+        /// <returns></returns>
+        public static string Join(this string baseString, string stringToJoin, string separator = "") {
+            return string.IsNullOrEmpty(baseString) ? stringToJoin : string.Join(separator, baseString, stringToJoin);
+        }
+    }
+}
