ed9aa811d0d6a8ef951c0ccfdcc23e9626621926
brandoncimino <brandon.cimino@gmail.com>
2020-08-15 09:44:31 -0500

    Removed 0.0001 test and fixed an assertion message typo

--- Assets/Tests/Runtime/IncrementalUtilsTests.cs
+++ Assets/Tests/Runtime/IncrementalUtilsTests.cs
@@ -16,55 +16,39 @@ namespace Tests.Runtime {
             12341.43123,
             0.001,
             97658765.0001,
-            0.0001,
             21.123456
         };
 
         [Test]
         [Combinatorial]
-        public void TestNumberOfTimesCompletedCombinatorial(
-            [ValueSource(nameof(seconds))] double deltaTimeInSeconds,
-            [ValueSource(nameof(seconds))] double timeToCompleteInSeconds
-        ) {
+        public void TestNumberOfTimesCompletedCombinatorial([ValueSource(nameof(seconds))] double deltaTimeInSeconds, [ValueSource(nameof(seconds))] double timeToCompleteInSeconds) {
             LogUtils.Log($"{nameof(deltaTimeInSeconds)} = {deltaTimeInSeconds} ~ {TimeUtils.NormalizeSeconds(deltaTimeInSeconds)}");
 
-            deltaTimeInSeconds = TimeUtils.NormalizeSeconds(deltaTimeInSeconds);
+            deltaTimeInSeconds      = TimeUtils.NormalizeSeconds(deltaTimeInSeconds);
             timeToCompleteInSeconds = TimeUtils.NormalizeSeconds(timeToCompleteInSeconds);
 
-            if (timeToCompleteInSeconds == 0) Assert.Ignore($"Failure conditions with a {nameof(timeToCompleteInSeconds)} of <= 9 are a different test!");
+            if (timeToCompleteInSeconds <= 0) {
+                Assert.Ignore($"Failure conditions with a {nameof(timeToCompleteInSeconds)} ({timeToCompleteInSeconds}) <= 0 are a different test!");
+            }
 
-            var deltaTime = TimeSpan.FromSeconds(deltaTimeInSeconds);
+            var deltaTime      = TimeSpan.FromSeconds(deltaTimeInSeconds);
             var timeToComplete = TimeSpan.FromSeconds(timeToCompleteInSeconds);
 
             var startTime = DateTime.Today;
-            var endTime = DateTime.Today.AddSeconds(deltaTimeInSeconds);
+            var endTime   = DateTime.Today.AddSeconds(deltaTimeInSeconds);
 
             LogUtils.Log($"test {nameof(startTime)} = {startTime} ({startTime.Ticks})");
             LogUtils.Log($"test {nameof(endTime)} = {endTime} ({endTime.Ticks})");
 
             var expectedTimesCompleted = Math.Floor(deltaTimeInSeconds / timeToCompleteInSeconds);
-            var modSeconds = deltaTimeInSeconds % timeToCompleteInSeconds;
+            var modSeconds             = deltaTimeInSeconds % timeToCompleteInSeconds;
             LogUtils.Log($"{deltaTimeInSeconds} % {timeToCompleteInSeconds} = {modSeconds}");
             var expectedLastCompletionTime = startTime.AddSeconds(timeToCompleteInSeconds * expectedTimesCompleted);
 
-            Assert.That(
-                IncrementalUtils.NumberOfTimesCompleted(
-                    startTime,
-                    endTime,
-                    timeToComplete,
-                    out var lastCompletionTime
-                ),
-                Is.EqualTo(expectedTimesCompleted)
-            );
+            Assert.That(IncrementalUtils.NumberOfTimesCompleted(startTime, endTime, timeToComplete, out var lastCompletionTime), Is.EqualTo(expectedTimesCompleted));
 
             //to account for minor inaccuracies in floating point math, this checks for a tolerance range
-            Assert.That(
-                lastCompletionTime,
-                Is.InRange(
-                    expectedLastCompletionTime.AddMilliseconds(-1),
-                    expectedLastCompletionTime.AddMilliseconds(1)
-                )
-            );
+            Assert.That(lastCompletionTime, Is.InRange(expectedLastCompletionTime.AddMilliseconds(-1), expectedLastCompletionTime.AddMilliseconds(1)));
         }
     }
 }
