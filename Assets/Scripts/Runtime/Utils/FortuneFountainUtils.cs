1e8e5f3bfe7aad4d81a10adc547c75a0e72f4ad7
brandoncimino <brandon.cimino@gmail.com>
2020-08-18 12:25:51 -0500

    Added FortuneFountainUtils.cs

---
+++ Assets/Scripts/Runtime/Utils/FortuneFountainUtils.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Runtime.Valuables;
+
+namespace Runtime.Utils {
+    /// <summary>
+    /// Contains extension methods and things specific to FortuneFountain stuff like <see cref="PlayerValuable"/>.
+    /// </summary>
+    public static class FortuneFountainUtils {
+        /// <summary>
+        /// Calls <see cref="PlayerValuable.CheckGenerate"/> against all <see cref="IDictionary{TKey,TValue}.Values"/> in <paramref name="playerValuables"/>.
+        ///
+        /// TODO: Can this be placed somewhere closer to <see cref="PlayerValuable"/>?
+        /// </summary>
+        /// <param name="playerValuables"></param>
+        public static void CheckGenerate<T>(this IDictionary<T, PlayerValuable> playerValuables) {
+            foreach (var pair in playerValuables) {
+                pair.Value.CheckGenerate();
+            }
+        }
+
+        /// <summary>
+        /// Calls <see cref="PlayerValuable.CheckGenerate"/> against all items in <paramref name="playerValuables"/>.
+        /// </summary>
+        /// <seealso cref="CheckGenerate{T}"/>
+        /// <param name="playerValuables"></param>
+        public static void CheckGenerate(this IEnumerable<PlayerValuable> playerValuables) {
+            foreach (var playerValuable in playerValuables) {
+                playerValuable.CheckGenerate();
+            }
+        }
+    }
+}
