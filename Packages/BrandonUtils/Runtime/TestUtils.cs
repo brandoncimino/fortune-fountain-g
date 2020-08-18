6d1ddc16b25b7e824a995ad4e4843c811d0c6249
brandoncimino <brandon.cimino@gmail.com>
2020-08-18 11:39:54 -0500

    - Added NUnit to the runtime .asmdef - Added support for "Has.All.Values()" via: -- AllValuesConstraint.cs -- ValuesOperator.cs

--- Packages/BrandonUtils/Runtime/TestUtils.cs
+++ Packages/BrandonUtils/Runtime/TestUtils.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections.Generic;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace Packages.BrandonUtils.Runtime
-{
-    public static class TestUtils
-    {
-        public static void AreEqual<T>(IList<T> expectedList, IList<T> actualList)
-        {
+namespace Packages.BrandonUtils.Runtime {
+    public static class TestUtils {
+        /// <summary>
+        /// Assert that <paramref name="expectedList"/> and <see cref="actualList"/> match <b>exactly</b>.
+        ///
+        /// TODO: Now that I know more about NUnit, can this be done with stuff like <see cref="NUnit.Framework.Constraints.CollectionConstraint"/>? If not, can this be converted to use the <see cref="ConstraintExpression"/> system?
+        /// </summary>
+        /// <param name="expectedList"></param>
+        /// <param name="actualList"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void AreEqual<T>(IList<T> expectedList, IList<T> actualList) {
             Assert.AreEqual(expectedList.Count, actualList.Count, "The lists weren't the same length!");
-            for (int i = 0; i < expectedList.Count; i++)
-            {
+            for (int i = 0; i < expectedList.Count; i++) {
                 Debug.Log($"Comparing {expectedList[i]} == {actualList[i]}");
                 Assert.AreEqual(expectedList[i], actualList[i], $"The lists differ at index [{i}]!");
             }
+        }
+
+        /// <summary>
+        /// An extension method, intended to be called against <see cref="NUnit.Framework.Has"/>, to apply the <see cref="AllValuesConstraint"/>.
+        /// </summary>
+        /// <param name="constraintExpression"></param>
+        /// <returns></returns>
+        public static ConstraintExpression Values(this ConstraintExpression constraintExpression) {
+            return constraintExpression.Append(new ValuesOperator());
         }
     }
 }
---
+++ Packages/BrandonUtils/Runtime/ValuesOperator.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework.Constraints;
+
+namespace Packages.BrandonUtils.Runtime {
+    /// <summary>
+    /// <see cref="CollectionOperator"/> that applies the <see cref="AllValuesConstraint"/>.
+    /// Analogous to <see cref="PropOperator"/>.
+    /// </summary>
+    public class ValuesOperator : CollectionOperator {
+        public override IConstraint ApplyPrefix(IConstraint constraint) => (IConstraint) new AllValuesConstraint(constraint);
+    }
+}
