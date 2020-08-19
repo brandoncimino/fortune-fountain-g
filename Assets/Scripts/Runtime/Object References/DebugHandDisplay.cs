8815460a675eb3006d81181cebe617212ce7dc65
brandoncimino <brandon.cimino@gmail.com>
2020-08-19 12:23:41 -0500

    Added a reference for debug ui elements

---
+++ Assets/Scripts/Runtime/Object References/DebugHandDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Runtime.Valuables;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runtime.Object_References {
+    public class DebugHandDisplay : MonoBehaviour {
+        public Text DebugHandText;
+        public Text DebugKarmaText;
+
+        public void Update() {
+            // DebugHandText.text = JsonUtility.ToJson(GameManager.SaveData.Hand, true);
+            UpdateDebugHandText();
+            UpdateDebugKarmaText();
+        }
+
+        private void UpdateDebugHandText() {
+            var lines = GameManager.SaveData.Hand.throwables.GroupBy(it => it.ValuableType).Select(FormatPlayerValuableGroup);
+            DebugHandText.text = string.Join("\n", lines);
+        }
+
+        private static string FormatPlayerValuableGroup(IGrouping<ValuableType, Throwable> grouping) {
+            return $"{grouping.Key}: {grouping.Count()} ({string.Join(", ", grouping.Select(it => it.ThrowValue))})";
+        }
+
+        private void UpdateDebugKarmaText() {
+            DebugKarmaText.text = $"Wallet: {GameManager.SaveData.Karma}\nHand: {GameManager.SaveData.Hand.KarmaInHand}";
+        }
+    }
+}
