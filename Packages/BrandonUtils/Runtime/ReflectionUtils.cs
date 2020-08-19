5a124734e86736ec46e516ac99f0becd4f81e4e5
brandoncimino <brandon.cimino@gmail.com>
2020-08-19 11:11:37 -0500

    Added a stubbed ReflectionUtils.cs

---
+++ Packages/BrandonUtils/Runtime/ReflectionUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace Packages.BrandonUtils.Runtime {
+    /// <summary>
+    /// Contains utilities for <see cref="System.Reflection"/>.
+    /// </summary>
+    public static class ReflectionUtils {
+        /// <summary>
+        /// Returns the <see cref="MethodInfo"/> for this method that <b><i>called <see cref="ThisMethod"/></i></b>.
+        /// </summary>
+        /// <returns></returns>
+        public static MethodInfo ThisMethod() {
+            throw new NotImplementedException("TBD - I need to stop getting distracted!");
+        }
+    }
+}
