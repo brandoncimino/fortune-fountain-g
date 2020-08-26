390925deba864b5348443772ec78383d23035207
brandoncimino <brandon.cimino@gmail.com>
2020-08-26 10:37:23 -0500

    Added TimeParadoxException.cs

---
+++ Packages/BrandonUtils/Runtime/Exceptions/TimeParadoxException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+using JetBrains.Annotations;
+
+namespace Packages.BrandonUtils.Runtime.Exceptions {
+    public class TimeParadoxException : BrandonException {
+        public TimeParadoxException() { }
+        protected TimeParadoxException([NotNull] SerializationInfo info, StreamingContext context) : base(info, context) { }
+        public TimeParadoxException(string message) : base(message) { }
+        public TimeParadoxException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
