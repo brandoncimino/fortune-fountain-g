ae5bd2ba5c428e58eaff14130168f29485f931e7
brandoncimino <brandon.cimino@gmail.com>
2020-09-02 10:28:40 -0500

    Added FortuneFountainException.cs

---
+++ Assets/Scripts/Runtime/FortuneFountainException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+using JetBrains.Annotations;
+using Packages.BrandonUtils.Runtime.Exceptions;
+
+namespace Runtime {
+    /// <summary>
+    /// An exception that means something specific to Fortune Fountain's game logic went wrong.
+    /// </summary>
+    public class FortuneFountainException : BrandonException {
+        public FortuneFountainException() { }
+        protected FortuneFountainException([NotNull] SerializationInfo info, StreamingContext context) : base(info, context) { }
+        public FortuneFountainException(string message) : base(message) { }
+        public FortuneFountainException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
