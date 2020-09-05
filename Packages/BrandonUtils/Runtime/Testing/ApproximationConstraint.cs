c76ac4217bca57780f2df4688cc46c82a91a4290
brandoncimino <brandon.cimino@gmail.com>
2020-09-04 23:14:43 -0500

    Formatted DateTimes in ApproximationConstraint.cs similarly to NUnit

--- Packages/BrandonUtils/Runtime/Testing/ApproximationConstraint.cs
+++ Packages/BrandonUtils/Runtime/Testing/ApproximationConstraint.cs
@@ -7,8 +7,13 @@ namespace Packages.BrandonUtils.Runtime.Testing {
         private readonly object Threshold;
         private          object MinValue => GetMinValue(ExpectedValue, Threshold);
         private          object MaxValue => GetMaxValue(ExpectedValue, Threshold);
+        private const    string NUnitDateTimeFormat = "yyyy-MM-dd HH:mm:ss.fff";
 
-        public override string Description => $"approximately equal to {ExpectedValue}\n\t{nameof(Threshold)} = {Threshold}\n\t{nameof(MinValue)} = {MinValue}\n\t{nameof(MaxValue)} = {MaxValue}";
+        public override string Description =>
+            $"{FormatObject(ExpectedValue)} (approximately)" +
+            $"\n\t{nameof(Threshold)} = {FormatObject(Threshold)}" +
+            $"\n\t{nameof(MinValue)} = {FormatObject(MinValue)}" +
+            $"\n\t{nameof(MaxValue)} = {FormatObject(MaxValue)}";
 
         public ApproximationConstraint(object expectedValue, object threshold) : base((IComparable) GetMinValue(expectedValue, threshold), (IComparable) GetMaxValue(expectedValue, threshold)) {
             ExpectedValue = expectedValue;
@@ -21,6 +26,20 @@ namespace Packages.BrandonUtils.Runtime.Testing {
 
         private static T GetMaxValue<T>(T expectedValue, T threshold) {
             return (dynamic) expectedValue + (dynamic) threshold;
+        }
+
+        /// <summary>
+        /// Formats <paramref name="obj"/> in a similar style to NUnit's "MsgUtils" (which, unfortunately, is an <see langword="internal"/> class...)
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        private static string FormatObject(object obj) {
+            switch (obj) {
+                case DateTime date:
+                    return date.ToString(NUnitDateTimeFormat);
+                default:
+                    return obj.ToString();
+            }
         }
     }
 }
