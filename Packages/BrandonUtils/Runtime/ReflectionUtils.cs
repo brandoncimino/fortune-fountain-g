d7846ff15d5556d8219cd07fadec4f20abcd2d33
brandoncimino <brandon.cimino@gmail.com>
2020-09-16 13:00:21 -0500

    Added some ReflectionUtils.cs and ReflectionUtilsTests.cs

--- Packages/BrandonUtils/Runtime/ReflectionUtils.cs
+++ Packages/BrandonUtils/Runtime/ReflectionUtils.cs
@@ -1,17 +1,97 @@
 ﻿using System;
 using System.Reflection;
 
+using JetBrains.Annotations;
+
+using Packages.BrandonUtils.Runtime.Exceptions;
+
 namespace Packages.BrandonUtils.Runtime {
     /// <summary>
     /// Contains utilities for <see cref="System.Reflection"/>.
     /// </summary>
     public static class ReflectionUtils {
+        public const BindingFlags VariablesBindingFlags =
+            BindingFlags.Default |
+            BindingFlags.Instance |
+            BindingFlags.NonPublic |
+            BindingFlags.Public |
+            BindingFlags.Static;
+
         /// <summary>
         /// Returns the <see cref="MethodInfo"/> for this method that <b><i>called <see cref="ThisMethod"/></i></b>.
         /// </summary>
         /// <returns></returns>
         public static MethodInfo ThisMethod() {
             throw new NotImplementedException("TBD - I need to stop getting distracted!");
+        }
+
+        [UsedImplicitly]
+        [Pure]
+        public static MemberInfo GetVariableInfo(this Type type, string variableName) {
+            var prop = type.GetProperty(variableName, VariablesBindingFlags);
+            if (!(prop is null)) {
+                return prop;
+            }
+
+            var field = type.GetField(variableName, VariablesBindingFlags);
+            if (!(field is null)) {
+                return field;
+            }
+
+            throw new NotImplementedException($"The {nameof(type)} {type} did not have a field or property named {variableName}!");
+        }
+
+        [Pure]
+        public static MemberInfo GetVariableInfo<T>(T obj, string variableName) {
+            return typeof(T).GetVariableInfo(variableName);
+        }
+
+        [Pure]
+        public static MemberInfo GetVariableInfo<T>(string variableName) {
+            return typeof(T).GetVariableInfo(variableName);
+        }
+
+        [UsedImplicitly]
+        [Pure]
+        public static T GetVariable<T>(object obj, string variableName) {
+            var v = obj.GetType().GetVariableInfo(variableName);
+            switch (v) {
+                case PropertyInfo prop:
+                    try {
+                        return (T) prop.GetValue(obj);
+                    }
+                    catch (InvalidCastException e) {
+                        throw new InvalidCastException($"A property named {variableName} was found for the {obj.GetType().Name} {obj}, but it couldn't be cast to a {typeof(T).Name}!", e);
+                    }
+                case FieldInfo field:
+                    try {
+                        return (T) field.GetValue(obj);
+                    }
+                    catch (InvalidCastException e) {
+                        throw new InvalidCastException($"A field named {variableName} was found for the {obj.GetType().Name} {obj}, but it couldn't be cast to a {typeof(T).Name}!", e);
+                    }
+            }
+
+            throw new BrandonException($"Couldn't find a field or property named {variableName} for the type {obj.GetType().Name}!");
+        }
+
+        [Pure]
+        public static object GetVariable(object obj, string variableName) {
+            return GetVariable<object>(obj, variableName);
+        }
+
+        public static void SetVariable<T>(object obj, string variableName, T value) {
+            var v = obj.GetType().GetVariableInfo(variableName);
+            switch (v) {
+                case PropertyInfo prop:
+                    prop.SetValue(obj, value);
+                    return;
+                case FieldInfo field:
+                    field.SetValue(obj, value);
+                    return;
+                default:
+                    throw new BrandonException($"Couldn't find a field or property named {variableName} for the type {obj.GetType()}!");
+            }
         }
     }
 }
---
+++ Packages/BrandonUtils/Tests/ReflectionUtilsTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using Packages.BrandonUtils.Runtime;
+
+// ReSharper disable MemberCanBePrivate.Local
+// ReSharper disable UnusedAutoPropertyAccessor.Local
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Local
+
+namespace Packages.BrandonUtils.Tests {
+    public class ReflectionUtilsTests {
+        private class Privacy<T> {
+            public    T Field_Public;
+            private   T Field_Private;
+            protected T Field_Protected;
+
+            public    T Prop_Public               { get;         set; }
+            private   T Prop_Private              { get;         set; }
+            protected T Prop_Protected            { get;         set; }
+            public    T Prop_Mixed_Private_Getter { private get; set; }
+            public    T Prop_Mixed_Private_Setter { get;         private set; }
+            public    T Prop_Get_Only             => Prop_Public;
+
+            public static    T Field_Static_Public;
+            private static   T Field_Static_Private;
+            protected static T Field_Static_Protected;
+
+            public static    T Prop_Static_Public               { get;         set; }
+            private static   T Prop_Static_Private              { get;         set; }
+            protected static T Prop_Static_Protected            { get;         set; }
+            public static    T Prop_Static_Mixed_Private_Getter { private get; set; }
+            public static    T Prop_Static_Mixed_Private_Setter { get;         private set; }
+            public static    T Prop_Static_Get_Only             => Prop_Static_Public;
+
+
+            public Privacy(T value) {
+                Field_Public    = value;
+                Field_Private   = value;
+                Field_Protected = value;
+
+                Prop_Public               = value;
+                Prop_Private              = value;
+                Prop_Protected            = value;
+                Prop_Mixed_Private_Getter = value;
+                Prop_Mixed_Private_Setter = value;
+
+                Field_Static_Public    = value;
+                Field_Static_Private   = value;
+                Field_Static_Protected = value;
+
+                Prop_Static_Public               = value;
+                Prop_Static_Private              = value;
+                Prop_Static_Protected            = value;
+                Prop_Static_Mixed_Private_Getter = value;
+                Prop_Static_Mixed_Private_Setter = value;
+            }
+
+            public static string[] VariableNames = {
+                nameof(Field_Public),
+                nameof(Field_Private),
+                nameof(Field_Protected),
+                nameof(Prop_Public),
+                nameof(Prop_Private),
+                nameof(Prop_Protected),
+                nameof(Prop_Mixed_Private_Getter),
+                nameof(Prop_Mixed_Private_Setter),
+                nameof(Field_Static_Public),
+                nameof(Field_Static_Private),
+                nameof(Field_Static_Protected),
+                nameof(Prop_Static_Public),
+                nameof(Prop_Static_Private),
+                nameof(Prop_Static_Protected),
+                nameof(Prop_Static_Mixed_Private_Getter),
+                nameof(Prop_Static_Mixed_Private_Setter),
+            };
+        }
+
+        public static string[] VariableNames = Privacy<int>.VariableNames;
+
+        [Test]
+        public void GetVariable(
+            [ValueSource(
+                nameof(VariableNames)
+            )]
+            [Values(
+                nameof(Privacy<object>.Prop_Get_Only),
+                nameof(Privacy<object>.Prop_Static_Get_Only)
+            )]
+            string variableName
+        ) {
+            var randomInt = new Random().Next();
+
+            var privacy = new Privacy<int>(randomInt);
+
+            var val = ReflectionUtils.GetVariable<int>(privacy, variableName);
+            Assert.That(val, Is.EqualTo(randomInt));
+        }
+
+        [Test]
+        public void SetVariable(
+            [ValueSource(nameof(VariableNames))]
+            string variableName
+        ) {
+            var randomInt = new Random().Next();
+            var setInt    = randomInt + 1;
+
+            var privacy = new Privacy<int>(randomInt);
+
+            ReflectionUtils.SetVariable(privacy, variableName, setInt);
+            Assert.That(ReflectionUtils.GetVariable(privacy, variableName), Is.EqualTo(setInt));
+            Assert.That(ReflectionUtils.GetVariable(privacy, variableName), Is.Not.EqualTo(randomInt));
+        }
+    }
+}
